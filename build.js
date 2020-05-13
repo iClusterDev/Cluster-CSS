504a73fc7a0625e94c0b5313de4785184de1a187
iClusterDev <cluster.coredev@gmail.com>
2020-05-13 20:55:47 +0100

    gulpfile added for build

--- build.js
+++
@@ -1,8 +0,0 @@
-const sass = require("sass");
-
-const compiled = sass.renderSync({
-  file: "./src/styles/test.scss",
-  outFile: "./dist/test.css",
-});
-
-console.log(compiled);
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, series } = require("gulp");
+const del = require("del");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("autoprefixer");
+
+/**
+ * clean the dist folder
+ * before any re-build
+ */
+const clean = async (cleanCB) => {
+  return del("./dist", { force: true });
+};
+
+/**
+ * compile the main.scss
+ * into a gigantic css file
+ * using dart-sass
+ */
+sass.compiler = require("sass");
+const scssCompile = () => {
+  return src("./src/styles/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./dist"));
+};
+
+/**
+ * exports the tasks
+ * as default tasks
+ */
+if (process.env.NODE_ENV === "production") {
+  exports.default = series(clean, scssCompile);
+} else {
+  exports.default = series(clean, scssCompile);
+}
